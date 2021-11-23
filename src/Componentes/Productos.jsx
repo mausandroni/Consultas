87f9b6b3f81c26487a2a6a6c7fddcd8136247207
Mauro Sandroni <msandroni@hotmail.com.ar>
2021-11-23 17:46:18 -0300

    corregimos las busquedas

---
+++ src/Componentes/Productos.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import AgregarProducto from './AgregarProducto';
+import Coleccion from './Coleccion';
+//import { useState } from 'react';
+function Productos() {
+    //const [categorias, setCategorias] = useState(['Gaseosas', 'Galletitas', 'Arroz', 'Azúcar']);
+    const [categorias, setCategorias] = useState([]);
+    // eslint-disable-next-line
+    /* const handleAgregar = () => {
+        //setCategorias([...categorias, 'Cervezas']);
+        
+        setCategorias(['Cervezas', ...categorias]);
+        //setCategorias(cat => [...cat, 'Cervezas']);
+    } */
+    
+    return (
+        <>
+            <h1>Productos</h1>
+            <AgregarProducto
+                setCategorias={setCategorias}
+            />
+            <ol>
+             {/*    {
+                    categorias.map(categoria => {
+                        return <li key={categoria}>{categoria}</li>
+                    })
+                } */}
+            </ol>
+            {/* <button 
+                onClick={handleAgregar} 
+                type="button" 
+                className="btn btn-secondary">
+                    Agregar Producto
+            </button> */}
+            <Coleccion categorias={categorias}/>
+        </>
+    )
+}
+export default Productos;
---
+++ src/Componentes/Titulo.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+
+const Titulo = () => {
+    return (
+        <div>
+            <h1>Mi app de Comunicacion</h1>
+        </div>
+    )
+}
+
+export default Titulo;
